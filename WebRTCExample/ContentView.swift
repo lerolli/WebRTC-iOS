705af22934778f037d6cfd6a920cdaf32343dcb1
Anton Zakharov <leroli.edu@gmail.com>
2023-11-09 11:28:14 +0500

    Initial Commit

---
+++ WebRTCExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebRTCExample
+//
+//  Created by User on 09.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebRTCExample/WebRTCExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebRTCExampleApp.swift
+//  WebRTCExample
+//
+//  Created by User on 09.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WebRTCExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
